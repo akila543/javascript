ea45c6388d1db9b3bc2a5addc040abfc19496758
kritiraj <kritirajsinghi@gmail.com>
2017-02-07 12:42:25 +0530

    stageScheduler test

---
+++ Pipeline/test/stageScheduler.spec.js
@@ -0,0 +1,104 @@
+const redis = require('redis');
+const async = require('async');
+const should = require('should');
+var client=redis.createClient(6379, '127.0.0.1');
+const retrieveStage = require('../Orchestrator/stateServices/stages/retrieveStage');
+const updateStage = require('../Orchestrator/stateServices/stages/updateStage');
+const updatePayload = require('../Orchestrator/stateServices/payload/updatePayload');
+
+var stageScheduler=require('../Orchestrator/stageScheduler')
+
+describe('StageScheduler', () => {
+  var input = {
+    jobId: 'CI-Pipeline_1',
+    stageName: 'build'
+  }
+  var stages =JSON.stringify({
+          type: "stackroute/javascript/build",
+          input: {
+              "WORKSPACE": "{{context.workspace}}"
+          },
+          output: null,
+          context: null,
+          depends_on: ["gitClone"],
+          status: "initialized"
+      });
+  var payload=JSON.stringify({
+     repo: "node-uuid",
+      author: "broofa",
+      branch: "master",
+      headCommitId: "0983kshjhaqi1123344kmdjnsj",
+      commitIds: "839290njnwyqiqka19238jsjwj111",
+      gitClone: ""
+  });
+  before(function(done) {
+     async.series([
+      updateStage.bind(null,input.jobId,input.stageName,stages),
+       updatePayload.bind(null,input.jobId,payload),
+       stageScheduler.bind(null,JSON.stringify(input)),
+     ],done);
+   });
+
+  it('status of stage is changed from initialized to scheduled and ts_scheduled property added', function(done) {
+    // TODO: Retrieve stage
+      async.waterfall([
+        retrieveStage.bind(null,input.jobId,input.stageName),
+        (stage, callback) => {
+          var stages=JSON.parse(stage);
+          should.exist(stages);
+          // TODO: stage should have property status and should be updated to scheduled
+          stages.should.have.property('status').and.be.exactly('scheduled');
+          // TODO: stage should have property ts_scheduled
+          stages.should.have.property('ts_scheduled');
+          callback();
+        }
+      ], done);
+
+
+  });
+
+    it('Pushing Back to the languagePack', function(done) {
+      // TODO: Retrieve message from jobScheduler
+      async.series([
+        function(callback){
+          client.llen('stackroute/javascript',function(err,length){
+            length.should.be.exactly(1);
+          });
+          callback();
+        },
+        function(callback){
+          client.lrange('stackroute/javascript',0,-1,function(err,reply){
+            // TODO: queue should have property jobId
+            var my_reply=JSON.parse(reply[0]);
+            my_reply.should.have.property('jobId');
+              should.exist(my_reply.jobId);
+            // TODO: queue should have property stageName
+            my_reply.should.have.property('stageName');
+              should.exist(my_reply.stageName);
+                    // TODO: queue should have property cmd
+            my_reply.should.have.property('cmd');
+              should.exist(my_reply.cmd);
+                      // TODO: queue should have property input
+            my_reply.should.have.property('input');
+            should.exist(my_reply.input);
+          });
+          callback();
+        }
+      ],done);
+      });
+
+
+  after(function(done) {
+    flushRedis(done);
+  });
+});
+
+function flushRedis(done) {
+client.flushdb(function(err,reply){
+  if(err)
+  {
+    console.log(err);
+  }
+});
+done();
+}
