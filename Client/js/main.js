c8ccd23442c52c12ec1834c1e9c18b72af355fe8
nisha uttawani <nisha.uttawani@wipro.com>
2017-02-17 17:23:39 +0530

    workflow

--- Client/js/main.js
+++ Client/js/main.js
@@ -4,7 +4,6 @@ import App from '../jsx/view/App.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import {Router,Route,hashHistory} from "react-router";
-import AddTemplate from "../jsx/component/AddTemplate.jsx";
 import Monitoring from "../jsx/component/Monitoring.jsx";
 import Dashboard from "../jsx/component/Dashboard.jsx";
 import WorkFlowEdit from "../jsx/component/WorkFlowEdit.jsx";
