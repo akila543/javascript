8520b48833beeb8731d7a5cb97302f5af3db38d4
Karanjeet Singh <karanjeetsingh298@gmail.com>
2017-02-23 15:43:45 +0530

    ui

--- Client/jsx/component/DashNavbar.jsx
+++ Client/jsx/component/DashNavbar.jsx
@@ -53,7 +53,7 @@ class DashNavbar extends React.Component {
 
             <div>
 
-                <AppBar title="Octopus" onLeftIconButtonTouchTap={this.handleToggle} iconElementRight={< Link to = "/" > <FlatButton label="Logout" onClick={this.handleLogout}/> < /Link>}/>
+                <AppBar title="Octopus" onLeftIconButtonTouchTap={this.handleToggle} iconElementRight={< Link to = "/" > <FlatButton label="Logout" onClick={this.handleLogout} style={{color:'white'}}/> < /Link>}/>
 
                 <Drawer docked={false} width={250} open={this.state.open} onRequestChange={(open) => this.setState({open})}>
 <Menu desktop={true} width={320}>
